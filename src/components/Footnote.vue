2adc4afa8c6a2c8e20f9872bb658595aa6c69aa9
bosterm <bostermeierm@gmail.com>
2021-07-10 15:48:52 -0500

    Create Footnote.vue
    Create footnote component, currently just has default properties.

---
+++ src/components/Footnote.vue
@@ -0,0 +1,17 @@
+<template>
+    <sup
+    ><a
+        class="reference"
+        id="reference1"
+        href="#footnote1"
+        title='Office of the Chancellor, "Land Acknowledgement Statement," 2018.'
+        >1</a
+    ></sup
+    >
+</template>
+
+<script>
+export default {
+  name: "Footnote",
+};
+</script>
