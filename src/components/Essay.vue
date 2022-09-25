ed7d9a1d24bfcd3b5bfb8e304eed775ade760dd4
bosterm <bostermeierm@gmail.com>
2022-09-24 20:09:01 -0500

    Update Essay.vue
    Load first footnote into essay using text replace and :is loader

--- src/components/Essay.vue
+++ src/components/Essay.vue
@@ -5,58 +5,36 @@
       the Doris Duke Oral History Collection
     </h1>
     <h2 id="byline">Ben Ostermeier</h2>
-    <article v-html="essay" />
-    <footnote
-    text='Office of the Chancellor, "Land Acknowledgement Statement," 2018, <a href="https://chancellor.illinois.edu/land_acknowledgement.html">https://chancellor.illinois.edu/land_acknowledgement.html</a>.'
-    fid="chancellor-1"
-    @reference-clicked="slideToElement"
-    :footnoteNumber="
+    <article v-bind:is="processedEssay" />
+    <footnote text='Office of the Chancellor, "Land Acknowledgement Statement," 2018, <a href="https://chancellor.illinois.edu/land_acknowledgement.html">https://chancellor.illinois.edu/land_acknowledgement.html</a>.' fid="chancellor-1" @reference-clicked="slideToElement" :footnoteNumber="
         footnotes.find((obj) => {
         return obj.fid === 'chancellor-1';
         }).footnoteNumber
-    "
-    @footnote-added="addFootnote"
-    ></footnote>
-    <footnote
-      text="James E. Davis, <em>Frontier Illinois</em>. (Bloomington: Indiana University Press, 1998), 198-199."
-      fid="davis-1"
-      @reference-clicked="slideToElement"
-      :footnoteNumber="
-        footnotes.find((obj) => {
-          return obj.fid === 'davis-1';
-        }).footnoteNumber
-      "
-      @footnote-added="addFootnote"
-    ></footnote>
-      <footer id="references">
-        <h2>References</h2>
-        <ol>
-          <div
-            tabindex="0"
-            v-for="footnote in footnotes"
-            :key="footnote.fid"
-            class="footnote"
-            :id="'footnote' + footnote.footnoteNumber"
-          >
-            <span class="footnote-number">
-              <button
-                :data-digit="footnote.digits"
-                @click="slideToElement('reference' + footnote.footnoteNumber)"
-                >{{ footnote.footnoteNumber }}</button
-              >
-            </span>
-            <cite
-              v-html="footnote.text"
-            ></cite>
-          </div>
-        </ol>
-      </footer>
+    " @footnote-added="addFootnote"></footnote>
+    <footnote text="James E. Davis, <em>Frontier Illinois</em>. (Bloomington: Indiana University Press, 1998), 198-199." fid="davis-1" @reference-clicked="slideToElement" :footnoteNumber="
+      footnotes.find((obj) => {
+        return obj.fid === 'davis-1';
+      }).footnoteNumber
+    " @footnote-added="addFootnote"></footnote>
+    <footer id="references">
+      <h2>References</h2>
+      <ol>
+        <div tabindex="0" v-for="footnote in footnotes" :key="footnote.fid" class="footnote" :id="'footnote' + footnote.footnoteNumber">
+          <span class="footnote-number">
+            <button :data-digit="footnote.digits" @click="slideToElement('reference' + footnote.footnoteNumber)">{{ footnote.footnoteNumber }}</button>
+          </span>
+          <cite v-html="footnote.text"></cite>
+        </div>
+      </ol>
+    </footer>
   </main>
 </template>
 
 <script>
 import Footnote from "./Footnote.vue";
 import externalData from '../assets/data.json';
+import Vue from 'vue';
+Vue.component('footnote', Footnote);
 
 export default {
   name: "Essay",
@@ -88,8 +66,17 @@ export default {
     },
     slideToElement(elementID) {
       var element = document.getElementById(elementID)
-      element.scrollIntoView({behavior: "smooth", block: "center"});
-      element.focus({preventScroll: true});
+      element.scrollIntoView({ behavior: "smooth", block: "center" });
+      element.focus({ preventScroll: true });
+    }
+  },
+  computed: {
+    processedEssay() {
+      let essay = this.essay.replace('[fid:chancellor-1]', '<footnote text=\'Office of the Chancellor, "Land Acknowledgement Statement," 2018, <a href="https://chancellor.illinois.edu/land_acknowledgement.html">https://chancellor.illinois.edu/land_acknowledgement.html</a>.\' fid="chancellor-1" @reference-clicked="slideToElement" :footnoteNumber="1"></footnote>');
+      return {
+        template: '<div>' + essay + '</div>'
+      }
+
     }
   }
 };
