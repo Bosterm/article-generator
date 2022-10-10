560ced74c7d96b99c14f1394b1f7540e6bbdcfd5
bosterm <bostermeierm@gmail.com>
2022-10-09 19:47:12 -0500

    Set up API

--- back-end/routes/api.php
+++ back-end/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EssayController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+Route::group(['prefix'=>'post'], function () {
+    Route::get('/', [EssayController::class, 'index']);
+    Route::get('/{id}', [EssayController::class, 'show']);
+    Route::post('/', [EssayController::class, 'store']);
+    Route::put('/{id}', [EssayController::class, 'update']);
+    Route::delete('/{id}', [EssayController::class, 'destroy']);
 });
