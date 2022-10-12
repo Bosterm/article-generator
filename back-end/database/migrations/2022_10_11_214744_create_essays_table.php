f885a82c8da1fd0994585bc286ac4eaf64d25bfb
bosterm <bostermeierm@gmail.com>
2022-10-11 21:21:43 -0500

    Change body column of essay table
    Made 'body' column of table use longtext instead of string

--- back-end/database/migrations/2022_10_11_214744_create_essays_table.php
+++ back-end/database/migrations/2022_10_11_214744_create_essays_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->string('slug');
-            $table->string('body');
+            $table->longText('body');
             $table->timestamps();
         });
     }
